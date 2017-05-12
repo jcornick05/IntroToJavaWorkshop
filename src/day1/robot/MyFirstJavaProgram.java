93b6525dbfefd0fed686bbfef23d01ee417f6248
test <league@WTS15.attlocal.net>
2017-05-12 16:59:57 -0700

    okay

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -6,10 +6,33 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
-		// START HERE
-
+		// START HERE 
+Robot Dave= new  Robot();
+Dave.setPenWidth(10);
+Dave.setSpeed(100);
+Dave.turn(-90);
+Dave.move(900);
+Dave.penDown();
+for (int i = 100; i < 100; i++) {
 	
-		
-		
+}
+Dave.turn(180);
+Dave.move(100);
+Dave.turn(270);
+Dave.move(300);
+Dave.turn(90);
+Dave.move(100);
+Dave.turn(90);
+Dave.move(300);
+Dave.turn(-90);
+Dave.move(100);
+Dave.turn(-90);
+Dave.move(175);
+Dave.turn(90);
+Dave.move(100);
+Dave.turn(90);
+Dave.move(175);
+
+
 	}
 }
